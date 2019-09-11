e2f89ca3c2c6c70017f86f9dc82a03e2d285a924
Luis Rozo <luisgonzagarozo@gmail.com>
2019-09-11 12:49:28 +0200

    add some session management

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animalsRouter = require('./routes/animals');
 
 var app = express();
+
+app.use(session({
+  secret: 'app nodejs',
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- backend/routes/animals.js
+++ backend/routes/animals.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var viewsCount = function(req, res, next) {
+    if(req.session.views === undefined) {
+        req.session.views = 0;
+    }
+
+    req.session.views++;
+    next();
+};
+
+router.use(viewsCount);
+
 router.get('/', function(req, res, next) {
-    res.send('Animals resource');
+    console.log(req.session);
+    res.send('Animals resource has been seen ' + req.session.views + ' times.');
 });
 
 module.exports = router;
