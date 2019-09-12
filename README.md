3c45090142ba5c67a429ef839fe4d01e15897fed
Luis Rozo <luisgonzagarozo@gmail.com>
2019-09-12 10:36:26 +0200

    set middleware

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This whole code has been developed in order to follow and understand an Udemy on
 
 You can find the following folders: 
 * ***client***: HTTP client implementation
+* ***backend***: some server-side code managed by Express package
 
 ### Success certification
 *Coming soon...*
--- backend/routes/animals.js
+++ backend/routes/animals.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+/* Content-type of each request must be application/json */
+
+var validation = function(req, res, next) {
+    if(req.get("content-type") === "application/json") {
+        console.log("Request validated");
+    } else {
+        res.status(403).send("ERROR");
+    }
+
+    next();
+};
+
 var viewsCount = function(req, res, next) {
     if(req.session.views === undefined) {
         req.session.views = 0;
@@ -10,6 +22,7 @@ var viewsCount = function(req, res, next) {
     next();
 };
 
+router.use(validation);
 router.use(viewsCount);
 
 router.get('/', function(req, res, next) {
